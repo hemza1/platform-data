45c1e75c78db9c40446bc7e2147897960337e5d0
Hemza1 <hamzaelyesri2017@gmail.com>
2026-02-13 14:02:34 +0100

    first dag

---
+++ airflow/dags/hello_dag.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+from airflow.sdk import dag, task
+
+
+@dag(
+    dag_id="hello_world_simple",
+    schedule="@daily",
+    start_date=datetime(2026, 1, 1),
+    catchup=False,
+    tags=["seance2", "taskflow"],
+)
+def hello_world_dag():
+    @task
+    def say_hello() -> str:
+        msg = "Hello, Airflow 3.x!"
+        print(msg)
+        return msg
+
+    @task
+    def consume_message(message: str) -> None:
+        print(f"Consumed message: {message}")
+
+    consume_message(say_hello())
+
+
+hello_world = hello_world_dag()
